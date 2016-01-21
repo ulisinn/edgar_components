2890b2a21938898231ef759bc0aa3469fc76df09
Ulrich Sinn <uli@addtothenoise.com>
2016-01-21 09:55:12 -0800

    import cleaned up

--- app/index.jsx
+++ app/index.jsx
@@ -4,14 +4,8 @@ require('./components/los_feliz/css/main.scss');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-import Main from './components/Main.js';
 import {Router} from 'react-router';
-
 import routes from "./components/los_feliz/config/routes";
-
-
-//ReactDOM.render(<Main />, document.getElementById('app'));
 
 main();
 
